b8268f4667291dc5a928d24e33fd9eb56ca5356d
john-masters <mastersjohnr@gmail.com>
2023-03-19 23:27:33 +1100

    Basic backend done

--- index.js
+++ index.js
@@ -2,16 +2,32 @@ import express from 'express'
 import cors from 'cors'
 import { Configuration, OpenAIApi } from "openai"
 import fs from "fs"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
+})
+
 const openai = new OpenAIApi(configuration);
-const resp = await openai.createTranscription(
-  fs.createReadStream("news.mp3"),
-  "whisper-1"
-);
-console.log(resp)
+
+const transcribe = await openai.createTranscription(
+  fs.createReadStream("news.mp3"), // file
+  "whisper-1", // model
+  "Please transcribe: ", // prompt
+  "text", //response_format
+  "0", // temperature
+  "en" // language (ISO-639-1 format)
+)
+
+const translate = await openai.createTranslation(
+  fs.createReadStream("japanese.mp3"), // file
+  "whisper-1", // model
+  "Please transcribe: ", // prompt
+  "text", //response_format
+  "0" // temperature
+)
 
 const app = express()
 
@@ -19,7 +35,7 @@ app.use(cors())
 
 app.use(express.json())
 
-app.get('/', (req, res) => res.send({info: resp}))
+app.get('/', (req, res) => res.send({info: transcribe.data}))
 
 const port = 8080
 
