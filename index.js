19256677c9da4a02160c88205a7b655899c2d44f
john-masters <mastersjohnr@gmail.com>
2023-03-22 00:09:37 +1100

    Changed to try/catch

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-import express from "express";
-import cors from "cors";
-import { Configuration, OpenAIApi } from "openai";
-import fs from "fs";
-import dotenv from "dotenv";
+import express from "express"
+import cors from "cors"
+import { Configuration, OpenAIApi } from "openai"
+import fs from "fs"
+import dotenv from "dotenv"
 
-dotenv.config();
+dotenv.config()
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
+})
 
-const app = express();
-const port = 8080;
+const openai = new OpenAIApi(configuration)
 
-app.use(cors());
+const app = express()
+const port = 8080
 
+app.use(cors())
 app.use(express.json({ limit: "25mb" }));
 
 app.listen(port, () =>
@@ -23,46 +23,25 @@ app.listen(port, () =>
 );
 
 app.post("/", async (req, res) => {
-  const { type, file, format } = req.body;
-  // change from binary
+  const { file } = req.body
 
-  // console.log(Buffer.from(file, "binary").toString("base64"))
-  console.log("req body", req.body)
+  // console.log(Buffer.from(file, "binary").toString("base64")) 
+  console.log("req.body", req.body)
   res.send()
+  try {
+    const res = await openai.createTranscription(
+      fs.createReadStream(file),
+      // Buffer.from(file, "binary").toString("base64"),
+      "whisper-1",
+      "",
+      "json",
+      "0",
+      "en"
+    )
+    res.send(res)
+  }
+  catch (error) {
+    res.send(error)
+  }
 
-  // if (type === "translate") {
-  //   await openai
-  //     .createTranslation(
-  //       // fs.createReadStream(file),
-  //       "whisper-1", // model
-  //       "", // prompt
-  //       format,
-  //       "0" // temperature
-  //     )
-  //     .then((response) => {
-  //       res.send(response.data);
-  //     })
-  //     .catch((error) => {
-  //       res.send(error);
-  //     });
-  // } else {
-  //   try {
-  //     const result = await openai
-  //     .createTranscription(
-  //       // fs.createReadStream(file),
-  //       Buffer.from(file, "binary").toString("base64"),
-  //       "whisper-1", // model
-  //       "", // prompt
-  //       format,
-  //       "0", // temperature
-  //       "en" // language (ISO-639-1 format)
-  //     )
-  //     console.log("result: ", result)
-  //     res.send(result)
-  //   }
-  //   catch(err) {
-  //     console.log("err: ", err.response.data)
-
-  //   }
-  // }
-});
+})
