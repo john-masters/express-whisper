4887406613f2265b32f7c1ed16ba8afe4f9ec0ca
john-masters <mastersjohnr@gmail.com>
2023-03-29 20:12:05 +1100

    Add root get req

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ app.listen(port, () =>
   console.log(`Server running at http://localhost:${port}`)
 );
 
+app.get("/", (req, res) => {
+  res.send("Hello World!")
+})
+
 app.post("/", upload.single('file'), async (req, res) => {
   const format = req.body.format
   const filePath = req.file.path
