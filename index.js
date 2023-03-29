6bbfd730b0fc88ba8f50648250ee5e6a8cf0fa2f
john-masters <mastersjohnr@gmail.com>
2023-03-29 19:57:45 +1100

    Updated port

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration)
 
 const app = express()
-const port = 8080
+const port = process.env.PORT || 8080
 
 app.use(cors())
 app.use(express.json({ limit: "25mb" }))
