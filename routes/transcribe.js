0f2a8e5b0aec19ee4000d19f8d417af4b14ccd7d
john-masters <mastersjohnr@gmail.com>
2023-04-26 22:05:15 +1000

    chunking files first attempt

--- routes/transcribe.js
+++ routes/transcribe.js
@@ -14,9 +14,53 @@ router.post("/", upload.single("file"), async (req, res) => {
     res.status(400).send({ message: "No file uploaded" });
     return;
   };
-  const fileStream = fs.createReadStream(filePath);
 
-  createTranscription(fileStream, format, filePath, res);
+  const fileSizeInMegabytes = fs.statSync(filePath).size / 1000000;
+
+  if (fileSizeInMegabytes > 25) {
+    console.log("File size exceeds 25 MB, splitting file");
+    const fileParts = await splitFile(filePath);
+
+    let combinedResult = "";
+
+    for (const filePart of fileParts) {
+      const fileStream = fs.createReadStream(filePart.path);
+      const result = await createTranscription(fileStream, format, filePart.path, res, false);
+      combinedResult += result;
+
+      // Delete the temporary part file
+      fs.unlinkSync(filePart.path);
+    }
+
+    sendResultAsResponse(combinedResult, format, filePath, res);
+
+  } else {
+    const fileStream = fs.createReadStream(filePath);
+    const result = await createTranscription(fileStream, format, filePath, res);
+
+    sendResultAsResponse(result, format, filePath, res);
+  }
 });
+
+function sendResultAsResponse(result, format, filePath, res) {
+  const extension = () => {
+    switch (format) {
+      case "text":
+        return ".txt"
+      case "srt":
+        return ".srt"
+      case "vtt":
+        return ".vtt"
+    }
+  };
+
+  const fileName = path.basename(filePath, path.extname(filePath)) + extension();
+
+  res.set({
+    "Content-Type": "Application/octet-stream",
+    "Content-Disposition": `attachment; filename=${fileName}`
+  });
+  res.send(Buffer.from(result))
+}
 
 export { router as transcribeRouter };
--- utils/openai.js
+++ utils/openai.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 import fs from "fs";
 import path from "path";
+import { v4 as uuidv4 } from "uuid";
 
 dotenv.config();
 
@@ -18,9 +19,10 @@ export const openai = new OpenAIApi(
   configuration, undefined, customAxiosInstance
 );
 
-export async function createTranscription(fileStream, format, filePath, res) {
+export async function createTranscription(fileStream, format, filePath, res, unlinkFile = true) {
   try {
     console.log("Transcription started")
+
     const result = await openai.createTranscription(
       fileStream,
       "whisper-1",
@@ -29,25 +31,10 @@ export async function createTranscription(fileStream, format, filePath, res) {
       "0",
       "en"
     );
+
     console.log("Transcription finished")
-    const extension = () => {
-      switch (format) {
-        case "text":
-          return ".txt"
-        case "srt":
-          return ".srt"
-        case "vtt":
-          return ".vtt"
-      }
-    };
+    return result.data;
 
-    const fileName = path.basename(filePath, path.extname(filePath)) + extension();
-
-    res.set({
-      "Content-Type": "Application/octet-stream",
-      "Content-Disposition": `attachment; filename=${fileName}`
-    });
-    res.send(Buffer.from(result.data))
   } catch (err) {
     console.log("error: ", err);
   } finally {
@@ -59,4 +46,26 @@ export async function createTranscription(fileStream, format, filePath, res) {
       console.log(`${filePath} was deleted`);
     });
   }
+}
+
+export async function splitFile(filePath) {
+  const maxPartSize = 25;
+  const fileSizeInMegabytes = fs.statSync(filePath).size / 1000000;
+
+  const partCount = Math.ceil(fileSizeInMegabytes / maxPartSize);
+
+  let fileParts = [];
+
+  for (let i = 0; i < partCount; i++) {
+    const partPath = `${filePath}-${uuidv4()}`;
+    const start = i * maxPartSize;
+    const end = Math.min((i + 1) * maxPartSize, fileSizeInMegabytes);
+
+    const buffer = fs.readFileSync(filePath).slice(start, end)
+    fs.writeFileSync(partPath, buffer);
+
+    fileParts.push({ path: partPath, start, end});
+  }
+
+  return fileParts;
 }
