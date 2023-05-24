ff572baa9213170bcf0405153312b00f136fb0d3
john-masters <mastersjohnr@gmail.com>
2023-05-24 20:47:04 +1000

    Update bitrate due to quality concerns

--- utils/compresser.js
+++ utils/compresser.js
@@ -1,24 +1,23 @@
-import ffmpeg from 'fluent-ffmpeg';
+import ffmpeg from "fluent-ffmpeg";
 
 export default async function compressFile(inputPath, outputPath) {
-
   return new Promise((resolve, reject) => {
     ffmpeg(inputPath)
       .noVideo()
       .audioFrequency(16000)
-      // .audioBitrate(32)
-      .audioBitrate(24)
+      .audioBitrate(32)
+      // .audioBitrate(24)
       .audioChannels(1)
-      .audioCodec('libmp3lame')
-      .format('mp3')
-      .on('error', (err) => {
-        console.error('An error occurred while converting the audio:', err);
+      .audioCodec("libmp3lame")
+      .format("mp3")
+      .on("error", (err) => {
+        console.error("An error occurred while converting the audio:", err);
         reject(err);
       })
-      .on('end', () => {
-        console.log('Conversion to 16k .mp3 completed');
+      .on("end", () => {
+        console.log("Conversion to 16k .mp3 completed");
         resolve(outputPath);
       })
       .save(outputPath);
   });
-};
+}
